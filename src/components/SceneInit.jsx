0d82ffa21fc5ce033b4047e8a15cdf3a91236e10
Joaquin <joaquinbozzani@gmail.com>
2023-07-25 23:22:08 -0300

    adding physics

--- src/components/SceneInit.jsx
+++ src/components/SceneInit.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect } from 'react';
+
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
+
+import * as CANNON from 'cannon-es';
+import CannonDebugger from 'cannon-es-debugger';
 
 const SceneInit = () => {
 
@@ -21,7 +26,7 @@ const SceneInit = () => {
           1000
       );
       camera.position.z = 96;
-      
+    
   
       //----- LIGHTS -----
       const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
@@ -43,7 +48,7 @@ const SceneInit = () => {
         objLoader.load('dog.obj', 
         // called when resource is loaded
         function(object) {
-          object.position.z -= 60;
+          console.log(object)
           scene.add(object); 
         },
         // called when loading is in progresses
@@ -57,9 +62,35 @@ const SceneInit = () => {
 
       })
 
-
-
       
+      // ----- PHYSICS -----
+      //world for simulated physics
+      const physicsWorld = new CANNON.World({
+        gravity: new CANNON.Vec3(0, -9.81, 0),
+      });
+      //time for physics
+      const timeStep = 1 / 60;
+
+      //create floor
+      const groundBody = new CANNON.Body({
+        type: CANNON.Body.STATIC,
+        //infinite ground
+        shape: new CANNON.Plane(),
+      });
+      //rotate ground by 90deg
+      groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+      physicsWorld.addBody(groundBody);
+
+      //create box to use as hitbox for models
+      const size = 1
+      const halfExtents = new CANNON.Vec3(size, size, size)
+      const boxShape = new CANNON.Box(halfExtents)
+      const boxBody = new CANNON.Body({ mass: 10, shape: boxShape })
+      boxBody.position.set(0, 10, 0)
+      physicsWorld.addBody(boxBody)
+
+
+
       //----- RENDERER -----
       const canvas = document.getElementById('webgl');
       const renderer =  new THREE.WebGLRenderer({
@@ -69,15 +100,29 @@ const SceneInit = () => {
       renderer.setSize(window.innerWidth, window.innerHeight);
       document.body.appendChild(renderer.domElement);
 
+
+      //----- CONTROLS ----- DELETE WHEN FINISHED -----
+      const controls = new OrbitControls( camera, renderer.domElement );
+      camera.position.set( 0, 20, 100 );
+      controls.update();
+
+
+      //----- CANNON DEBUGGER ----- DELETE WHEN FINISHED -----
+      const cannonDebugger = new CannonDebugger(scene, physicsWorld, {
+        // options...
+      })
   
+
       //----- ANIMATE -----
       const animate = () => {
-        renderer.render(scene, camera);
-        window.requestAnimationFrame(animate);
+        physicsWorld.step(timeStep); //update physics
+        cannonDebugger.update() // Update the CannonDebugger meshes
+        renderer.render(scene, camera); //render the threejs scene
+        window.requestAnimationFrame(animate); 
       };
       animate();
   
-   
+
       // ----- RESIZE -----  
       window.addEventListener('resize', () => {
         camera.aspect = window.innerWidth / window.innerHeight;
